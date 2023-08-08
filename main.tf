ec974f66c4376f705df867f5ca38004561b9f1e4
Goto Ippei <paaaech@gmail.com>
2023-08-08 14:19:55 +0900

    fmt

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "aws-terraform-ecs-tfstate"
-    key = "terraform.tfstate"
-    region = "ap-northeast-1"
+    bucket         = "aws-terraform-ecs-tfstate"
+    key            = "terraform.tfstate"
+    region         = "ap-northeast-1"
     dynamodb_table = "aws-terraform-ecs-tfstate-locking"
-    encrypt = true
+    encrypt        = true
   }
 }
